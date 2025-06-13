083683cc069a9292ef009c25b95417ed936d020c
Дмитрий Тращенко <setworldspawn@gmail.com>
2025-06-13 03:28:23 +0300

    Формирование приложения tasks проекта

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "priority", "deadline", "completed")
+    list_filter = ("priority", "completed")
+    search_fields = ("title", "tags")
