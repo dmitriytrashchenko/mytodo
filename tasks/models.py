d0700093c967dd88cda5f9699c5e849be15c60da
Дмитрий Тращенко <setworldspawn@gmail.com>
2025-06-13 03:09:13 +0300

    Написал модель задачи

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("L", "Low"),
+        ("M", "Medium"),
+        ("H", "High"),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    tags = models.CharField(
+        max_length=100, blank=True
+    )  # для простоты, можно позже улучшить
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default="M")
+    deadline = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    order = models.PositiveIntegerField(default=0)  # для drag'n'drop сортировки
+
+    def __str__(self):
+        return self.title
